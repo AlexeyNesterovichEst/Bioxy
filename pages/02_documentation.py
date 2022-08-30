778483dbecf37bef61d538e97d08475f5ef7cee2
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-08-30 09:42:58 +0300

    Update 02_documentation.py

--- pages/02_documentation.py
+++ pages/02_documentation.py
@@ -10,32 +10,32 @@ if option == "Sequence": # search by name, GenBank accession number
     c1 = st.container()
     c1.subheader("Gene sequence")
     c1.write("[taxon] [GENE NAME] (start-end) sequence")
-    c1.markdown("*Example: human DMBT1 1-100 sequence*")
+    c1.markdown("*Example: human MAGEA10 1-10 sequence*")
     #c1.text("")
     #c1.write("[taxon] [Gene name] (start-end) gene sequence")
     #c1.markdown("*Example: human Dmbt1 1-100 gene sequence*")
     #c1.text("")
     c1.write("[GenBank accession number] (start-end) accession sequence")
-    c1.markdown("*Example: AB020851.2 1-100 accession sequence*")
+    c1.markdown("*Example: AY522506.1 1-10 accession sequence*")
     
 # improve 
     c2 = st.container()
     c2.subheader("Protein sequence")
     c2.write("[taxon] [Protein name] (start-end) protein sequence")
-    c2.markdown("*Example: human Deleted in malignant tumor 1 1-100 protein sequence*")
+    c2.markdown("*Example: human Melanoma-associated antigen 10 1-10 protein sequence*")
     
     c2.write("[GenBank accession number] (start-end) protein sequence")
-    c2.markdown("*Example: AB020851.2 1-100 protein sequence*")
+    c2.markdown("*Example: NP_066386.3 1-10 protein sequence*")
     
     c3 = st.container()
     c3.subheader("Gene coding sequence")
     c3.write("[GenBank accession number] (start-end) gene coding sequence") # check start_end
-    c3.markdown("*Example: AB020851.2 1-100 accession coding sequence*") 
+    c3.markdown("*Example: AY522506.1 1-10 accession coding sequence*") 
     
     c4 = st.container()
     c4.subheader("Protein coding sequence")
-    c4.write("[taxon] [Protein name] (start-end) protein coding sequence")
-    c4.markdown("*Example: human Deleted in malignant tumor 1 1-100 protein coding sequence*")
+    c4.write("[GenBank accession number] (start-end) protein coding sequence")
+    c4.markdown("*Example: human NP_066386.3 1-10 protein coding sequence*")
 
 # develop [GenBank accession number] (start-end) primers
 if option == "Primers":
