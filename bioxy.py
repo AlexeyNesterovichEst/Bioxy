c84cf6375c24a0ab7b5cc1bbe295b067b4974165
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-09-01 10:59:13 +0300

    Update bioxy.py

--- bioxy.py
+++ bioxy.py
@@ -9,6 +9,7 @@ from pydna.design import primer_design
 from Bio.Seq import Seq
 from Bio.Restriction import *
 
+
 # combine gene and prot_term_query
 # create interactive functions' graph and list functions by appearance in code
 # add search by identifier and read from xml more accurate source
@@ -175,7 +176,18 @@ def gene_seq(gene_term,tax_sci,d1,d2,opt):
                         with tab3:
                               exp_text = "%s %s coding sequence (%s)" % (tax_sci,data[seq_i-n],source)
                               with st.expander(exp_text):
-                                  st.code(s)   
+                                  st.code(s)
+            if opt == "primer":
+                a_f = recs[0].features
+                for f in a_f:
+                    if f.type == "CDS":
+                        s = f.location.extract(recs[0]).seq
+                        if d1 != "" and d2 != "":
+                          if d1 == "1":
+                             s = s[:(int(d2)*3)]
+                          else:
+                             s = s[((int(d1)-1)*3):(int(d2)*3)]
+                        return s
 
 def prot_term_query(tax,prot):
     a_tax = [["Homo sapiens","human"], ["Oryctolagus cuniculus","rabbit"],["Mus","Mus musculus", "mouse"]]
@@ -286,7 +298,6 @@ def ncbiprot_seq(prot_term,tax_sci,d1,d2,opt):
                       with st.expander(exp_text):
                           st.code(s)
             elif opt == "acc_cds":
-                #test different proteins
                 handle = Entrez.efetch(db="protein", id=recs[0].id,rettype="gb")
                 seq_record = SeqIO.read(handle, "genbank")
                 seqAnn = seq_record.annotations
@@ -301,7 +312,63 @@ def ncbiprot_seq(prot_term,tax_sci,d1,d2,opt):
                 gene = ""
                      #s = s[((int(d1)-1)*3):(int(d2)*3)]
                 gene_seq(gene_acc,"",d1,d2,"prot_cds") #acc_cds # d1,d2
-  return
+            elif opt == "primer":
+                handle = Entrez.efetch(db="protein", id=recs[0].id,rettype="gb")
+                seq_record = SeqIO.read(handle, "genbank")
+                seqAnn = seq_record.annotations
+                gene_acc = seqAnn['db_source'] #test different proteins
+                gene_acc_split = gene_acc.split()
+                gene_acc = gene_acc_split[-1]
+                dna = gene_seq(gene_acc,"",d1,d2,"primer")
+                t=Dseqrecord(dna)
+                ampl = primer_design(t, target_tm=55.0)
+                if d1 != "" and d2 != "":
+                    d1_d2 = d1 + "-" + d2
+                else:
+                    d1_d2 = ""
+                with tab1:
+                    st.code(ampl.forward_primer.seq)
+                    st.code(ampl.reverse_primer.seq)
+                with tab2:
+                    st.success("%s %s %s sequence " % (tax,protein,d1_d2))
+                    st.code(s)
+                    #coding sequence
+                    st.success("forward primer of %s %s %s" % (tax,protein,d1_d2)) # add temperature
+                    st.code(ampl.forward_primer.seq)
+                    st.success("reverse primer of %s %s %s" % (tax,protein,d1_d2)) # add temperature
+                    st.code(ampl.reverse_primer.seq)
+                with tab3:
+                    st.code(ampl.forward_primer.seq)
+                    st.code(ampl.reverse_primer.seq)
+                    #st.write("%s %s coding gene is %s with coding sequence %s (UniProt Consortium, 2021; NCBI Resource Coordinators ,2016)." % (tax.capitalize(),recs[0].description,gene,s))
+                    #st.write("%s %s (NCBI Resource Coordinators, 2016) primers are %s and %s." 
+                             #% (tax.capitalize(),gene, ampl.forward_primer.seq, ampl.reverse_primer.seq))
+            elif opt == "res_primer":
+                handle = Entrez.efetch(db="protein", id=recs[0].id,rettype="gb")
+                seq_record = SeqIO.read(handle, "genbank")
+                seqAnn = seq_record.annotations
+                gene_acc = seqAnn['db_source'] #test different proteins
+                gene_acc_split = gene_acc.split()
+                gene_acc = gene_acc_split[-1]
+                dna = gene_seq(gene_acc,"",d1,d2,"primer")
+                t=Dseqrecord(dna)
+                ampl = primer_design(t, target_tm=55.0)
+                and_i = try_except("and",data)
+                if and_i != -1:
+                    e1 = data[and_i+1]
+                    e2 = data[and_i-1]
+                else:
+                    e1 = data[prim_i+2]
+                    e2 = e1
+                re_s = eval(e1+ ".site")
+                fe_s = eval(e2+".site")
+                rp_e = re_s + ampl.reverse_primer.seq
+                fp_e = fe_s + ampl.forward_primer.seq
+                st.success("forward primer with %s site" % e2)
+                st.code(fp_e)
+                st.success("reverse primer with %s site" % e1)
+                st.code(rp_e)
+
 
 def prot_gene_seq(protein,tax_id,tax):
   r = get_url(f"{WEBSITE_API}/uniprotkb/search?query=(protein_name:{protein}) AND (taxonomy_id:{tax_id})&fields=gene_names", headers={"Accept": "text/plain; format=tsv"})
@@ -345,18 +412,22 @@ if(st.button('Submit')):
         tax_id = 9606
     if seq_i != -1:
         n,d1,d2 = partial(1)
-        if data[seq_i-n].isupper() == True: # "recognise gene sequence:  n,d1,d2 = partial(2),  if hum_i == -1:
+        if data[seq_i-n] == "gene":
+            if hum_i == -1:
+                n,d1,d2 = partial(2)
+                if d1 != -1:
+                    acc = data[seq_i-n] 
+                else:
+                    acc = data[seq_i-n-3] 
+                gene = ""
+                gene_seq(acc,"",d1,d2,"acc")
+            else:
+               st.success("gene sequence")
+               #look below and apply
+        if data[seq_i-n].isupper() == True: #  
             gene = data[seq_i-n]
             gene_term,tax_sci = gene_term_quiry(tax,gene)
             gene_seq(gene_term,tax_sci,d1,d2,"seq")
-        elif data[seq_i-n] == "accession":
-            n,d1,d2 = partial(2)
-            if d1 != -1:
-                acc = data[seq_i-n] 
-            else:
-                acc = data[seq_i-n-3] 
-            gene = ""
-            gene_seq(acc,"",d1,d2,"acc")
         elif data[seq_i-1] == "coding":
             n,d1,d2 = partial(3)
             if data[seq_i-2] == "gene":
@@ -388,10 +459,17 @@ if(st.button('Submit')):
                 protein = ' '.join(prot)
                 prot_term,tax_sci = prot_term_query(tax,protein)
                 ncbiprot_seq(prot_term,tax_sci,d1,d2,"seq")
-    elif prim_i != -1: #synchronise
-        st.success("primers")
+    elif prim_i != -1: 
+        n,d1,d2 = partial(2)
         if  data[prim_i-1] == "protein":
-            st.success("protein primers")
+            protein = data[0]
+            if data[prim_i] == data[-1]:
+                ncbiprot_seq(protein,"",d1,d2,"primer") 
+            if data[prim_i+1] == "with":
+                ncbiprot_seq(protein,"",d1,d2,"res_primer")
+        # add overhang    
+        elif data[prim_i-1] == "gene":
+            st.success("gene primers")
             
     else:
         st.error("No result")
