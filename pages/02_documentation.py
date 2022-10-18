7e9ce3afd03de098b5169316db762de3fd693053
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-10-18 22:39:41 +0300

    Update 02_documentation.py

--- pages/02_documentation.py
+++ pages/02_documentation.py
@@ -15,7 +15,7 @@ if option == "Sequence": # search by name, GenBank accession number
     #c1.write("[taxon] [Gene name] (start-end) gene sequence")
     #c1.markdown("*Example: human Dmbt1 1-100 gene sequence*")
     #c1.text("")
-    c1.write("[GenBank accession number] (start-end) gene sequence")
+    c1.write("[GenBank accession number of gene] (start-end) gene sequence")
     c1.markdown("*Example: NM_021048.5 1-10 gene sequence*")
     
  
@@ -24,7 +24,7 @@ if option == "Sequence": # search by name, GenBank accession number
     c2.write("[taxon] [Protein name] (start-end) protein sequence")
     c2.markdown("*Example: human Melanoma-associated antigen 10 1-10 protein sequence*")
     c2.text("")
-    c2.write("[GenBank accession number] (start-end) protein sequence")
+    c2.write("[GenBank accession number of protein] (start-end) protein sequence")
     c2.markdown("*Example: NP_066386.3 1-10 protein sequence*")
     
     
@@ -35,7 +35,7 @@ if option == "Sequence": # search by name, GenBank accession number
     
     c4 = st.container()
     c4.subheader("Protein coding sequence")
-    c4.write("[GenBank accession number] (start-end) protein coding sequence")
+    c4.write("[GenBank accession number of protein] (start-end) protein coding sequence")
     c4.markdown("*Example: NP_066386.3 1-10 protein coding sequence*")
 
 # develop [GenBank accession number] (start-end) primers
@@ -47,34 +47,33 @@ if option == "Primers":
     
     c5 = st.container()
     c5.subheader("Protein coding sequence primers")
-    c5.write("[GenBank accession number] (start-end) primers")
+    c5.write("[GenBank accession number of protein] (start-end) primers")
     c5.markdown("*Example: NP_066386.3 1-10 protein primers*")
     
     c6 = st.container()
     c6.subheader("Protein coding sequence primers with restriction sites")
-    c6.write("[GenBank accession number] (start-end) primers with [restrictase 1] (and restrictase 2)")
+    c6.write("[GenBank accession number of protein] (start-end) protein primers with [restrictase 1] (and restrictase 2)")
     c6.markdown("*Example: NP_066386.3 1-10 protein primers with Acc65I and HindIII*")
     
 if option == "Restriction":
     c7 = st.container()
-    c7.header("Protein coding sequence")
     c7.subheader("All restrictases with sites")
-    c7.write("[GenBank accession number] (start-end) protein restriction")
+    c7.write("[GenBank accession number of protein] (start-end) protein restriction")
     c7.markdown("*Example: NP_066386.3 1-100 protein restriction*")
     
     c8 = st.container()
     c8.subheader("Specific restrictases with sites")
-    c8.write("[GenBank accession number] (start-end) protein restriction with [restrictase 1] (, ... , restrictase n)")
+    c8.write("[GenBank accession number of protein] (start-end) protein restriction with [restrictase 1] (, ... , restrictase n)")
     c8.markdown("*Example: NP_066386.3 1-100 protein restriction with BseMII , UnbI*")
     
     c9 = st.container()
     c9.subheader("Restriction sequences for specific restrictases with sites")
-    c9.write("[GenBank accession number] (start-end) protein restriction sequence with [restrictase 1] (, ... , restrictase n)")
+    c9.write("[GenBank accession number of protein] (start-end) protein restriction sequence with [restrictase 1] (, ... , restrictase n)")
     c9.markdown("*Example: NP_066386.3 1-100 protein restriction sequence with BseMII , UnbI*")
     
     c10 = st.container()
     c10.subheader("Restriction gel for specific restrictases with sites")
-    c10.write("[GenBank accession number] (start-end) protein restriction gel with [restrictase 1] (, ... , restrictase n)")
+    c10.write("[GenBank accession number of protein] (start-end) protein restriction gel with [restrictase 1] (, ... , restrictase n)")
     c10.markdown("*Example: NP_066386.3 1-100 protein restriction gel with AcsI*")
     #c11 = st.container()
     #c11.subheader("Restriction map specific restrictases with sites")
