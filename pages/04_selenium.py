2f3978f2a8e16c6f5faf8193eaf99f44d0511e05
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-10-19 09:36:07 +0300

    Update 04_selenium.py

--- pages/04_selenium.py
+++ pages/04_selenium.py
@@ -1,74 +1,63 @@
-import glob
-import os
-
+# import streamlit and other libraries
 import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
+from requests_html import HTMLSession
+import pandas as pd
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-features=NetworkService")
-options.add_argument("--window-size=1920x1080")
-options.add_argument("--disable-features=VizDisplayCompositor")
+# give our application a title
+st.title("Real-time web scraper with Python")
 
 
-def delete_selenium_log():
-    if os.path.exists('selenium.log'):
-        os.remove('selenium.log')
+# add our web scraping code
+session = HTMLSession()
+main_url = "http://books.toscrape.com/"
+main_page = session.get(main_url)
+
+navlinks = "div.side_categories>ul.nav.nav-list>li>ul>li>a"
+genres = [element.text for element in main_page.html.find(navlinks)]
+list_urls = [
+    f"{main_url}/{element.attrs['href']}" for element in main_page.html.find(navlinks)
+]
+genre_urls = dict(zip(genres, list_urls))
 
 
-def show_selenium_log():
-    if os.path.exists('selenium.log'):
-        with open('selenium.log') as f:
-            content = f.read()
-            st.code(content)
+@st.cache
+def data_extract(genre):
+    webpage = genre_urls.get(genre)
+    webpage = session.get(webpage)
+    urls = [
+        element.attrs["href"].strip("../")
+        for element in webpage.html.find("div.image_container>a")
+    ]
+
+    titles = [element.attrs["title"] for element in webpage.html.find("h3>a")]
+
+    imgs = [
+        element.attrs["src"].strip("../")
+        for element in webpage.html.find("div.image_container>a>img")
+    ]
+
+    ratings = [
+        element.attrs["class"][-1] for element in webpage.html.find("p.star-rating")
+    ]
+
+    prices = [element.text for element in webpage.html.find("p.price_color")]
+
+    availability = [element.text for element in webpage.html.find("p.instock")]
+
+    data = dict(
+        Title=titles,
+        URL=urls,
+        SourceImage=imgs,
+        Rating=ratings,
+        Price=prices,
+        Availability=availability,
+    )
+
+    return pd.DataFrame(data).to_markdown(index=False)
 
 
-# not required anymore:
-# def get_chromedriver_path():
-#     results = glob.glob('/**/chromedriver', recursive=True)  # workaround on streamlit sharing
-#     return results[0]
+# add a sidebar to select genre
+option = st.sidebar.selectbox("Genres", genres)
 
-
-def run_selenium():
-    name = str()
-    with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
-        url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
-        driver.get(url)
-        xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
-        # Wait for the element to be rendered:
-        element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
-        name = element[0].get_property('attributes')[0]['name']
-    return name
-
-
-if __name__ == "__main__":
-    delete_selenium_log()
-    st.set_page_config(page_title="Selenium Test", page_icon='✅',
-        initial_sidebar_state='collapsed')
-    st.title('🔨 Selenium Test for Streamlit Sharing')
-    st.markdown("""
-        This app is only a very simple test for **Selenium** running on **Streamlit Sharing** runtime. <br>
-        The suggestion for this demo app came from a post on the Streamlit Community Forum.  <br>
-        <https://discuss.streamlit.io/t/issue-with-selenium-on-a-streamlit-app/11563>  <br>
-        In rare cases this app has deployment issues on Streamlit Cloud and the deployment fails, but usually it works.
-
-        This is just a very very simple example and more a proof of concept.
-        A link is called and waited for the existence of a specific class and read it. If there is no error message, the action was successful.
-        Afterwards the log file of chromium is read and displayed.
-
-        ---
-        """, unsafe_allow_html=True)
-
-    st.balloons()
-    if st.button('Start Selenium run'):
-        st.info('Selenium is running, please wait...')
-        result = run_selenium()
-        st.info(f'Result -> {result}')
-        st.info('Successful finished. Selenium log file is shown below...')
-        show_selenium_log()
+# add a line of code to show the result
+st.markdown(data_extract(option), unsafe_allow_html=True)
