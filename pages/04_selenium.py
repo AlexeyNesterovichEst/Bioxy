cf69035c87b8f32b171e080267209c725329c7a2
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-10-19 11:26:45 +0300

    Update 04_selenium.py

--- pages/04_selenium.py
+++ pages/04_selenium.py
@@ -1,3 +1,4 @@
+#https://github.com/wolfgangB33r/ai-text-model-studio/blob/main/src/app.py
 import streamlit as st
 import streamlit.components.v1 as components
 import numpy as np
@@ -98,34 +99,35 @@ max_links = st.slider('Maximum number of links to follow', 0, 100, 1)
 scraping_url = st.text_area('Url to scrape texts from (e.g.: texts from the book Pride And Prejudice)', 'https://www.gutenberg.org/cache/epub/1342/pg1342.html')
 
 scrapebutton = st.button('Start scraping')
-    
+
+a_id = []
+page = requests.get("http://addgene.org/search/catalog/plasmids/?page_number=1&page_size=10&q=pqm")
+parser = BeautifulSoup(page.text, 'html.parser')
+text = str(parser)
+#st.success(text)
+for line in text.split('\n'):
+    if '<div class="col-xs-10">#' in line:
+       line = line.strip()
+       ## example: <div class ="col-xs-10" >  # 107251</div>
+       id = line.split('#')[1].split('</div>')[0]
+       a_id.append(id)
+st.success(a_id[0])
+path_template = 'http://www.addgene.org/{}/sequences/'
+path = path_template.format(a_id[0])
+page = requests.get(path)
+parser = BeautifulSoup(page.text, 'html.parser')
+list_of_attributes = {"class": "copy-from form-control"}
+tags = parser.findAll('textarea', attrs=list_of_attributes)
+for tag in tags:
+    tag = tag.text
+    lines = tag.split('\n')
+    ref = lines[0].strip()
+    lines = lines[1:]
+    dna = ('').join(lines).strip()
+    st.success(dna.upper())
 
 if scrapebutton:
     st.session_state['url_scrape_stats'] = []
     urls = scraping_url.splitlines()
     st.session_state['sentences'] = []
-    a_id = []
-    page = requests.get("http://addgene.org/search/catalog/plasmids/?page_number=1&page_size=10&q=pqm")
-    parser = BeautifulSoup(page.text, 'html.parser')
-    text = str(parser)
-    #st.success(text)
-    for line in text.split('\n'):
-        if '<div class="col-xs-10">#' in line:
-           line = line.strip()
-           ## example: <div class ="col-xs-10" >  # 107251</div>
-           id = line.split('#')[1].split('</div>')[0]
-           a_id.append(id)
-    st.success(a_id[0])
-    path_template = 'http://www.addgene.org/{}/sequences/'
-    path = path_template.format(a_id[0])
-    page = requests.get(path)
-    parser = BeautifulSoup(page.text, 'html.parser')
-    list_of_attributes = {"class": "copy-from form-control"}
-    tags = parser.findAll('textarea', attrs=list_of_attributes)
-    for tag in tags:
-        tag = tag.text
-        lines = tag.split('\n')
-        ref = lines[0].strip()
-        lines = lines[1:]
-        dna = ('').join(lines).strip()
-        st.success(dna.upper())
+    
