ba3752a048c8a25c20092a825d4a1f2ca73a6d4b
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-10-19 11:30:26 +0300

    Update 04_selenium.py

--- pages/04_selenium.py
+++ pages/04_selenium.py
@@ -17,18 +17,6 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urlparse
 
-def inject(tag):
-    with open(os.path.dirname(st.__file__) + "/static/index.html", 'r') as file:
-        str = file.read()
-        if str.find(tag) == -1:
-            idx = str.index('<head>')
-            new_str = str[:idx] + tag + str[idx:]
-            with open(os.path.dirname(st.__file__) + "/static/index.html", 'w') as file:
-                file.write(new_str)
-
-st.session_state['stopwords'] = {'also', 'often', 'may', 'use', 'within', 'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'} 
-st.session_state['ignore_stop_words'] = True
-
 a_id = []
 page = requests.get("http://addgene.org/search/catalog/plasmids/?page_number=1&page_size=10&q=pqm")
 parser = BeautifulSoup(page.text, 'html.parser')
