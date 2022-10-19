244f3e3e89808496c531f915723e1e4455222a6e
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-10-19 11:36:27 +0300

    Update 04_selenium.py

--- pages/04_selenium.py
+++ pages/04_selenium.py
@@ -7,6 +7,7 @@ a_id = []
 plasmid = "pqm"
 path_template = "http://addgene.org/search/catalog/plasmids/?page_number=1&page_size=10&q={}"
 path = path_template.format("http://addgene.org/search/catalog/plasmids/?page_number=1&page_size=10&q=pqm")
+path = "http://addgene.org/search/catalog/plasmids/?page_number=1&page_size=10&q=" + plasmid
 page = requests.get(path)
 parser = BeautifulSoup(page.text, 'html.parser')
 text = str(parser)
