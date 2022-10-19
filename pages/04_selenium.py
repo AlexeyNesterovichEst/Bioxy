eb02c835c2d13714089ad375c3a27d025786cc00
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-10-19 09:17:34 +0300

    Update 04_selenium.py

--- pages/04_selenium.py
+++ pages/04_selenium.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-setuid-sandbox')
-chrome_options.add_argument('--remote-debugging-port=9222')
-chrome_options.add_argument('--disable-extensions')
-chrome_options.add_argument('start-maximized')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--headless')
+options.add_argument('--disable-setuid-sandbox')
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--disable-extensions')
+options.add_argument('start-maximized')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
 total_page = 1
 page_size = 10
 
