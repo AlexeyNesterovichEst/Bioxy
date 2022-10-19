e1e699b2a47e964e6273797be82df3263faba9b7
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-10-19 09:22:29 +0300

    Update 04_selenium.py

--- pages/04_selenium.py
+++ pages/04_selenium.py
@@ -1,50 +1,74 @@
+import glob
+import os
+
+import streamlit as st
 from selenium import webdriver
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--disable-setuid-sandbox')
-options.add_argument('--remote-debugging-port=9222')
-options.add_argument('--disable-extensions')
-options.add_argument('start-maximized')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
-total_page = 1
-page_size = 10
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
 
-a_id = []
 
-driver = webdriver.Chrome('chromedriver',options=options)
+def delete_selenium_log():
+    if os.path.exists('selenium.log'):
+        os.remove('selenium.log')
 
-plasmid = "pqm" #@param {type:"string"}
 
-path_template = 'http://addgene.org/search/catalog/plasmids/?page_number={}&page_size={}&q={}'
+def show_selenium_log():
+    if os.path.exists('selenium.log'):
+        with open('selenium.log') as f:
+            content = f.read()
+            st.code(content)
 
-for i in range(1,(total_page+1)):
-    driver.get(path_template.format(i, page_size,plasmid))
-    page = driver.page_source
-    for line in page.split('\n'):
-        if '<div class="col-xs-10">#' in line:
-            line = line.strip()
-            ## example: <div class ="col-xs-10" >  # 107251</div>
-            id = line.split('#')[1].split('</div>')[0]
-            a_id.append(id)
-print(a_id[0])
-from selenium import webdriver
-from bs4 import BeautifulSoup
 
-path_template = 'http://www.addgene.org/{}/sequences/'
+# not required anymore:
+# def get_chromedriver_path():
+#     results = glob.glob('/**/chromedriver', recursive=True)  # workaround on streamlit sharing
+#     return results[0]
 
-path = path_template.format(a_id[0])
-driver.get(path)
-page = driver.page_source
-parser = BeautifulSoup(page, "lxml")
-list_of_attributes = {"class": "copy-from form-control"}
-tags = parser.findAll('textarea', attrs=list_of_attributes)
-for tag in tags:
-    tag = tag.text
-    lines = tag.split('\n')
-    ref = lines[0].strip()
-    lines = lines[1:]
-    dna = ('').join(lines).strip()
-    st.success(dna.upper())
+
+def run_selenium():
+    name = str()
+    with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
+        url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+        driver.get(url)
+        xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
+        # Wait for the element to be rendered:
+        element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
+        name = element[0].get_property('attributes')[0]['name']
+    return name
+
+
+if __name__ == "__main__":
+    delete_selenium_log()
+    st.set_page_config(page_title="Selenium Test", page_icon='✅',
+        initial_sidebar_state='collapsed')
+    st.title('🔨 Selenium Test for Streamlit Sharing')
+    st.markdown("""
+        This app is only a very simple test for **Selenium** running on **Streamlit Sharing** runtime. <br>
+        The suggestion for this demo app came from a post on the Streamlit Community Forum.  <br>
+        <https://discuss.streamlit.io/t/issue-with-selenium-on-a-streamlit-app/11563>  <br>
+        In rare cases this app has deployment issues on Streamlit Cloud and the deployment fails, but usually it works.
+
+        This is just a very very simple example and more a proof of concept.
+        A link is called and waited for the existence of a specific class and read it. If there is no error message, the action was successful.
+        Afterwards the log file of chromium is read and displayed.
+
+        ---
+        """, unsafe_allow_html=True)
+
+    st.balloons()
+    if st.button('Start Selenium run'):
+        st.info('Selenium is running, please wait...')
+        result = run_selenium()
+        st.info(f'Result -> {result}')
+        st.info('Successful finished. Selenium log file is shown below...')
+        show_selenium_log()
